e663f3d46cfe7bb1992d89e06be5041b2b9b35b0
EgorKozlov <egor43585@bk.ru>
2021-08-23 15:49:06 +0300

    create project and add Car entity with CRUD methods

--- src/main/resources/db/migration/V1_0__create_tables.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `project`.`car` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `model` VARCHAR(45) NOT NULL,
-  `number` VARCHAR(45) NOT NULL,
-  `fuel` INT NOT NULL,
-  `milage` INT NOT NULL,
-  PRIMARY KEY (`id`));
